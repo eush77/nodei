a37810712a402d617b37a9eb9bbc404d1074db33
Eugene Sharygin <eush77@gmail.com>
2014-12-12 20:32:12 +0300

    Init

---
+++ bin/cli.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+'use strict';
+
+var nodei = require('..');
+
+var parseArgs = require('minimist');
+
+
+var usage = function () {
+  console.log('Usage:  nodei <filename>');
+  process.exit(1);
+};
+
+var argv = (function () {
+  var argv = parseArgs(process.argv.slice(2), {
+    unknown: function (arg) {
+      if (arg[0] == '-') {
+        usage();
+      }
+    }
+  });
+
+  if (argv._.length != 1) {
+    return usage();
+  }
+
+  return argv;
+}());
+
+
+nodei(argv._[0], function (err, repl) {
+  if (err) throw err;
+});
---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var extend = require('extend');
+
+var repl = require('repl')
+  , vm = require('vm')
+  , fs = require('fs');
+
+
+/**
+ * Start the REPL and expose variables in `context`.
+ *
+ * @arg {Object} context
+ * @return {Repl}
+ */
+var replWith = function (context) {
+  var r = repl.start({
+    prompt: '> ',
+    useGlobal: true
+  });
+
+  extend(r.context, context);
+  return r;
+};
+
+
+/**
+ * Start the REPL and expose top-level definitions in `filename`.
+ *
+ * @arg {string} filename
+ * @arg {function(Error, Repl)} [cb]
+ */
+module.exports = function (filename, cb) {
+  cb = cb || function () {};
+
+  fs.readFile(filename, { encoding: 'utf8' }, function (err, script) {
+    if (err) return cb(err);
+
+    var sandbox = {};
+    vm.runInNewContext(script, sandbox);
+
+    var repl = replWith(sandbox);
+    return cb(null, repl);
+  });
+};
