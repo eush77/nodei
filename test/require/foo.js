1e0e8a07f039e4ee1e541ebe778ad2badd0e457e
Eugene Sharygin <eush77@gmail.com>
2015-03-09 04:17:08 +0300

    Don't interpret any patterns with string.replace

--- test/require/foo.js
+++ test/require/foo.js
@@ -4,4 +4,9 @@ var chars = require('./chars.json'),
     lowercase = require('./lower-case'),
     string = require('./string');
 
+// This token triggers an error if the whole bundle replaces some
+// part of the template via simple String.prototype.replace call
+// with a string replacer.
+'$';
+
 module.exports = string(lowercase(chars));
